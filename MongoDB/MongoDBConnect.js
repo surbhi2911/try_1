f579e5e950cf8ec2845fa975731fd34686e937c4
surbhi <surbhipansuriya633@gmail.com>
2024-10-10 15:36:48 +0530

    remove 111

--- MongoDB/MongoDBConnect.js
+++ MongoDB/MongoDBConnect.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 
 const MongoDB = () => {
-    mongoose.connect('mongodb+srv://vaishalitankonly4loginsites:doo5sFH5BilUYK9Y@blooddata.vpp2o.mongodb.net/blood111?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://vaishalitankonly4loginsites:doo5sFH5BilUYK9Y@blooddata.vpp2o.mongodb.net/blood?retryWrites=true&w=majority')
         .then(() => {
             console.log('MongoDB Connected successfully!')
         });
