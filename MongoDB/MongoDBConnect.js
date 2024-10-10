1d59d96aa81224cf4acb8d7d0949d6e332761be1
surbhi <surbhipansuriya633@gmail.com>
2024-10-10 17:09:02 +0530

    increase time out

--- MongoDB/MongoDBConnect.js
+++ MongoDB/MongoDBConnect.js
@@ -1,24 +1,14 @@
 
 import mongoose from 'mongoose';
-const PAGE_SIZE = 1000;
-const MongoDB = async () => {
-    const { page } = req.query;
-    const pageNumber = parseInt(page) || 0;
 
-    mongoose.connect('mongodb+srv://vaishalitankonly4loginsites:doo5sFH5BilUYK9Y@blooddata.vpp2o.mongodb.net/blood111?retryWrites=true&w=majority')
+const MongoDB = () => {
+    mongoose.connect('mongodb+srv://vaishalitankonly4loginsites:doo5sFH5BilUYK9Y@blooddata.vpp2o.mongodb.net/blood111?retryWrites=true&w=majority'), {
+        socketTimeoutMS: 60000,  // 1 minute
+        connectTimeoutMS: 60000 // 1 minute
+    }
         .then(() => {
             console.log('MongoDB Connected successfully!')
         });
-    const collection = client.db("blood111").collection("donations");
-
-    const data = await collection
-        .find({})
-        .skip(pageNumber * PAGE_SIZE)
-        .limit(PAGE_SIZE)
-        .toArray();
-
-    res.status(200).json(data);
 }
-
 
 export default MongoDB;
